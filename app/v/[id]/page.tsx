1b6d1117a0ea33263480a34fbe177858ba880617
meisajoiri <meisajoiri@gmail.com>
2025-05-07 11:43:08 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -153,7 +153,7 @@ export default async function Video({ params }: PageProps) {
         {/* ... */}
         </section>
             <iframe
-                className="w-full h-[80vh] md:h-[80vh] lg:h-[80vh]"
+                className="w-full h-[85vh] md:h-[85vh] lg:h-[85vh]"
                 src={`https://doodstream.com/e/${file.filecode}`}
                 scrolling="no"
                 title={file.title}
